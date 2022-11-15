d85ad8b0287f562b2500200aa3d4c8678d4baeb9
AhmadJanuar1 <ahmadjanuaramri2015@gmail.com>
2022-11-15 19:07:12 +0700

    first commit

---
+++ app/src/main/java/com/ahmadjanuar/ui/theme/Type.kt
@@ -0,0 +1,18 @@
+package com.ahmadjanuar.ui.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.ahmadjanuar.R
+
+
+val montserratFamily = FontFamily(
+    Font(R.font.montserrat_light, FontWeight.Light),
+    Font(R.font.montserrat_medium, FontWeight.Medium),
+    Font(R.font.montserrat_bold, FontWeight.Bold)
+)
+
+val Typography = Typography(
+    defaultFontFamily = montserratFamily
+)
